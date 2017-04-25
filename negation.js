da118429f4aaddd6821517081b0d31a23dfbbf8c
SiamKing <thesiameseelectric@gmail.com>
2017-04-24 19:28:16 -0700

    refactored

--- negation.js
+++ negation.js
@@ -1,4 +1,3 @@
 function negationValue(string, value) {
-  let c = string.split('').length;
-  return c & 1 ? !Boolean(value) : Boolean(value);
+  return string.length & 1 ? !Boolean(value) : Boolean(value); 
 }
